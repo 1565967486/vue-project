7ff16005a1a24c1c56f8231bebb0c4c590243d3e
clx0220 <8430934+clx0220@user.noreply.gitee.com>
2025-03-19 10:32:28 +0800

    添加 Element Plus 依赖，配置样式和自动导入，更新应用结构

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 配置element-plus采用的sass样式配色系统
+        ElementPlusResolver({importStyle: 'sass'}),
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
     },
   },
 })
